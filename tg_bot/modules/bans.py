70ce55f9fd84d602a73144a4d98e1237056075c0
Maverick <maver_ck@outlook.com>
2019-03-26 05:23:21 +0800

    yui: better detailed /help modules
    * inspired from Rose
    
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- tg_bot/modules/bans.py
+++ tg_bot/modules/bans.py
@@ -362,6 +362,11 @@ def sban(bot: Bot, update: Update, args: List[str]) -> str:
     return ""
 
 __help__ = """
+Some people need to be publicly banned; spammers, annoyances, or just trolls.
+
+This module allows you to do that easily, by exposing some common actions, so everyone will see!
+
+
  - /banme: bans the user who issued the command.
  - /kickme: kicks the user who issued the command
 
@@ -371,6 +376,15 @@ __help__ = """
  - /unban <userhandle>: unbans a user. (via handle, or reply)
  - /sban <userhandle>: silently bans a user. (via handle, or reply)
  - /kick <userhandle>: kicks a user, (via handle, or reply)
+
+An example of temporarily banning someone:
+`/tban @username 2h`; this bans a user for 2 hours.
+
+As for the silent ban, it shows no bot output results on banning a user. \
+That makes admins of chat quietly ban users without any expose.
+
+An example of banning someone silently:
+`/sban @username or 728762378`; this bans user silently and deletes command.
 """
 
 __mod_name__ = "Bans"
--- tg_bot/modules/muting.py
+++ tg_bot/modules/muting.py
@@ -188,6 +188,9 @@ __help__ = """
  - /mute <userhandle>: silences a user. Can also be used as a reply, muting the replied to user.
  - /tmute <userhandle> x(m/h/d): mutes a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
  - /unmute <userhandle>: unmutes a user. Can also be used as a reply, muting the replied to user.
+ 
+ An example of temporarily banning someone:
+`/tmute @username 2h`; this mutes a user for 2 hours.
 """
 
 __mod_name__ = "Muting"
